ffc1d1b8f7ab5f4e722fc52da3486ede021989b5
allania7med11 <allania7med11@gmail.com>
2021-03-11 21:45:30 +0100

    exemple3

--- exemple3.js
+++ exemple3.js
@@ -1,21 +1,20 @@
 function getMinPos(arr) {
-    minPos = 0;
-    for (var i = 1; i < arr.length; i++) {
-      if (arr[i] < arr[minPos]) {
+  minPos = 0;
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[minPos]) {
       minPos = i;
-      }
-      return minPos;
     }
+  return minPos;
   }
-  function sort(arr) {
-    var rtn = [];
-    debugger
-    while (arr.length > 0) {
-      var minPos = getMinPos(arr);
-      rtn.push(arr[minPos]);
-      arr.splice(minPos, 1);
-    }
-    return rtn;
+}
+function sort(arr) {
+  var rtn = [];
+  while (arr.length > 0) {
+    var minPos = getMinPos(arr);
+    rtn.push(arr[minPos]);
+    arr.splice(minPos, 1);
   }
-  
-  console.log(sort([8, 4, 5, 1]));
+  return rtn;
+}
+
+console.log(sort([8, 4, 5, 1]));
